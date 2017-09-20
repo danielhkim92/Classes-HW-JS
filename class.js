a3e77013178557b377dbe24e9f6bac98e1d16706
danielhkim92 <danielhkim92@gmail.com>
2017-09-20 16:29:03 -0500

    commit 2

--- class.js
+++ class.js
@@ -6,3 +6,26 @@ class Pet {
 	this.owner = owner;
 }
 }
+
+class Dog extends Pet{
+	constructor(price, name){
+	super(name);
+	this.price = price;
+	}
+	bark(){
+		console.log('bark')
+	}
+	chaseTail(){
+		console.log('oh boy oh boy oh boy')
+	}
+	getPrice(){
+		console.log(this.price)
+	}
+}
+
+const sparky = new Dog(200, "Sparky")
+console.log(sparky.setOwner('Jim'))
+console.log(sparky)
+sparky.bark()
+sparky.chaseTail()
+sparky.getPrice()
